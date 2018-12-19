9ca884762a6f73a33d5220e7c1512694c1281024
Tommy Sætre <tommy.saetre@hotmail.com>
2018-12-19 08:36:08 +0100

    Update ptp.c

--- ptp.c
+++ ptp.c
@@ -2,9 +2,10 @@
 
 int main()
 {
-	int var   = 5;
-	int* p    = &var;
-	int** ptp = &p;
+	int var      = 5;
+	int* p       = &var;
+	int** ptp    = &p;	// Points to 'p'
+	int* sharedp = p;	// Does NOT point to 'p'. It shares 'p's address
 
 	printf("%i%c", **ptp, '\n');
 
