db34473d112cdd8ac1ef833dc1ef571d36c1f812
Tommy Sætre <tommy.saetre@hotmail.com>
2018-12-19 08:39:02 +0100

    Update swap.c

--- swap.c
+++ swap.c
@@ -13,14 +13,14 @@ int main()
 	
 	printf("After swapping values:\na: %i\nb: %i\n", a, b);
 
-	return 1;
+	return 0;
 }
 
 void swap(int* a, int* b)
 {
-	int tempVal;
+	int tempVal;	// Allocate space in the stack for a temporary variable
 
-	tempVal = *a;
-	*a = *b;
-	*b = tempVal;
+	tempVal = *a;	// Store a's value by dereferencing, we use a temp var to assign over 'a' without losing its value
+	*a = *b;	// Assign b's value to a
+	*b = tempVal;	// Assign a's value to b
 }
